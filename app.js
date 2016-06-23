98d352a6cb060426b9e84f64048cb58275215c57
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-06-22 21:54:12 -0600

    add middleware, and session keys

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('cookie-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+require('dotenv').load();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2] }))
+
 
 app.use('/', routes);
 app.use('/users', users);
